a0bd1e5ce844743ea090c396290ae1e066cead63
cswaroop <cswaroop@gmail.com>
2019-05-14 20:18:42 +0530

    first commit

---
+++ src/pranadata/core.clj
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Disaster Risk Management ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Adopted from Sendaiframework
+;;
+;; Goals:
+;;
+;; To adopt a concise, focused, forward-looking and action-oriented post 2015 framework for disaster
+;; risk reduction;
+
+;; To complete the assessment and review of the Hyogo framework for action 2005-2015:
+;; Building the Resilience of Nations and Communities to Disasters
+
+;; To consider the experience gained through the regional and national strategies/institutions
+;; and plans for disaster risk reduction and their recommendations, as well as relevant
+;; regional agreements for the implementation of the Hyogo Framework for Action;
+
+;; To identify modalities of cooperation based on committments to implement
+
+;; To determine the modalities for the periodic review of the implemenation of post
+;; 2015 framework for disaster risk reduction
+
+(ns pranadata.core)
+
---
+++ test/pranadata_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns pranadata-server.core-test
+  (:require [clojure.test :refer :all]
+            [pranadata-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
